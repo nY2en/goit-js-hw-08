0e43b148755caa1eecd174da638a2f9e6ceb5c7d
Nikita Chebanenko <chebanikita@icloud.com>
2023-04-02 13:39:21 +0300

    complete first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-
 console.log(galleryItems);
+
+function createGalleryMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                        <a class="gallery__link" href="${original}">
+                            <img class="gallery__image" src="${preview}" alt="${description}" />
+                        </a>
+                    </li>`;
+    })
+    .join('');
+}
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', onGalleryChange);
+
+function onGalleryChange(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+}

2a7ad5e9e097cc032456b292e9eb16c36f250910
Nikita Chebanenko <chebanikita@icloud.com>
2023-04-02 17:16:37 +0300

    prettier

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 import Player from '@vimeo/player';
-import throttle  from 'lodash.throttle';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
 
 const onPlay = function ({seconds}) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
